50d84d67fc6a2350245716170361e6592322cc94
Kuldeep tripathi <tkuldeep271@gmail.com>
2024-12-10 09:48:24 -0500

    done

--- js/account.js
+++ js/account.js
@@ -1,58 +1,9 @@
 
 
-let bigImg = document.querySelector('.big-img img');
-function showImg(pic){
-    bigImg.src = pic;
-}
-
-const decreaseButton = document.getElementById('decrease');
-const increaseButton = document.getElementById('increase');
-const numberDisplay = document.getElementById('number');
-
-let count = 0;
-
-decreaseButton.addEventListener('click', () => {
-    if (count > 0) {
-        count--;
-        numberDisplay.textContent = count;
-    }
-});
-
-increaseButton.addEventListener('click', () => {
-    if (count < 5) {
-        count++;
-        numberDisplay.textContent = count;
-    }
-});
-
-
-const videoContainers = document.querySelectorAll('.image-container');
-
-videoContainers.forEach(container => {
-  const video = container.querySelector('video');
-
-  container.addEventListener('mouseenter', () => {
-    video.style.display = 'block';
-    video.play();
-  });
-
-  container.addEventListener('mouseleave', () => {
-    video.pause();
-    video.style.display = 'none';
-  });
-});
-
-
-
-
-
-
-
-
-setTimeout(() => {
-  const topTextElement = document.getElementById('toptext');
-  topTextElement.textContent = "20% OFF on orders over ₹10,000 | Code: DIWALI10";
-}, 2000); 
+// setTimeout(() => {
+//   const topTextElement = document.getElementById('toptext');
+//   topTextElement.textContent = "20% OFF on orders over ₹10,000 | Code: DIWALI10";
+// }, 2000); 
 
 document.getElementById('heart-container').addEventListener('click', function () {
   const whiteHeart = document.getElementById('white-heart');
